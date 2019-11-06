597b8dc2e05dc743414c57b7216d5f8ff5e701c2
Amal S Raj <amalsujatharaj@gmail.com>
2019-11-06 14:52:28 +0000

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Iphone 11 pro max',
+    price: 1299,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Iphone 11 pro',
+    price: 999,
+    description: 'A great phone in small size'
+  },
+  {
+    name: 'Iphone 11',
+    price: 799,
+    description: 'Simple But Strong'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
